388b0d80e7cdce028b914e27fce369a02b212615
Leokanette <ettenakoel@gmail.com>
2024-07-14 20:53:09 +0800

    edit statusbox

--- src/pages/system_admin/components/statusbox.jsx
+++ src/pages/system_admin/components/statusbox.jsx
@@ -7,16 +7,19 @@ const StatusBox = ({ deptHed }) => {
 
         <div className='bg-blue-100 mt-8 m-2 p-4 text-white font-bold'>
             <div className='flex items-center'>
-            <img src={deptIcon} alt="Department Icon" />
-            <p className='font-thin text-custom-blue'>BUILDING AND GROUNDS MAINTENANCE SECTION</p>
+                <img src={deptIcon} alt="Department Icon" />
+                <p className='font-thin text-custom-blue'>BUILDING AND GROUNDS MAINTENANCE SECTION</p>
             </div>
-            <p className='mt-4 text-black'>Director/Head</p>
-            <div className='border rounded w-1/4 p-2 bg-gray-300'></div>
 
+            <p className='mt-4 text-black'>Director/Head</p>
+            
+            <div className='border rounded w-1/4 p-2 bg-gray-300'></div>
             <p className='mt-4 text-black'>Assistant Head</p>
             <div className='border rounded w-1/4 p-2 bg-gray-300'></div>
-            <div className='flex '>
-                <div>Activate</div>
+            <div className='p-2 '>
+                <div className='m-2 bg-green-500 text-white rounded '>Activate</div>
+                <div className='m-2 bg-red-500 text-white rounded '>Deactivate</div>
+                <div className='m-2 bg-yellow-500 text-white rounded '>Update</div>
                
             </div>
         </div>
