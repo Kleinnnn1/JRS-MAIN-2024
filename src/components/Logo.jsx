ec16f7d910b2c43f0725109a58934375287b5f0b
Karen Cadalo <82696971+Krunxx@users.noreply.github.com>
2024-08-16 09:38:35 +0800

    Delete src directory

--- src/components/Logo.jsx
+++
@@ -1,5 +0,0 @@
-import LogoJRS from "/src/assets/images/logo_JRS.png";
-
-export default function Logo() {
-  return <img src={LogoJRS} alt="Logo" className="w-[5-vh] h-[15vh]" />;
-}
--- src/pages/department_head/Profile/SaveChangesButton.jsx
+++
@@ -1,7 +0,0 @@
-export default function SaveChangesButton() {
-  return (
-    <button className="bg-blue-400 p-3 rounded-lg mr-5 text-white">
-      Save Changes
-    </button>
-  );
-}
