fa42bb082d065f1db20917f3712a5a01a18819e4
Leokanette <ettenakoel@gmail.com>
2024-08-18 13:31:30 +0800

    Department View

--- src/pages/system_admin/Department/ViewDepartment.jsx
+++ src/pages/system_admin/Department/ViewDepartment.jsx
@@ -1,37 +1,36 @@
 import { Edit } from "@mui/icons-material";
+import ReportTab from "./ReportTable";
 
 
 export default function ViewingDepartment (){
     return(
-        <div className="m-5">
-           
-     <div className="flex justify-between"> 
+    <div className="m-5"> 
+     <div className="flex justify-between m-10"> 
         
-        <div>
-        <p>
-          <b>Department/Office: </b>
+        <div >
+        <div className="bg-custom-blue text-yellow-500 font-sans text-md rounded p-4">
           Mechanical And Electrical Works Section
-        </p>
-        <p>
-          <b>Head/Director: </b>
+        </div>
+        <p className="p-2">
+          <b >Head/Director: </b>
           Thomas Xxxxx
         </p>
        
-        <p>
+        <p className="p-2">
           <b>Location: </b>
           DRER Right side
         </p>
-        <p>
+        <p className="p-2">
           <b>Email: </b>
           mews@gmail.com
         </p>
         </div>
         
         <div className="flex">
-          <b>Status: </b>
-          <b className="h-6 bg-green-500 text-white rounded font-bold text-xs p-1 mr-8">Active</b>
+          <b >Status: </b>
+          <b className="h-6 bg-green-500 text-white rounded font-bold text-xs p-1 ml-2 mr-8">Active</b>
           <button className="flex text-center text-xs  h-6">
-            <Edit className=" "/>
+            <Edit/>
         </button>
         </div>
        
@@ -40,8 +39,9 @@ export default function ViewingDepartment (){
       
         <div className="absolute bottom-4 right-4 flex">
         </div>
-            <div className="border "></div>
-
-        </div>
+    <div className="border "></div>
+    <ReportTab/>
+    </div>
+        
     );
 }
