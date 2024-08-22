92df66def83460a63270e0827007907d53333622
Raphael0603 <127576400+Raphael0603@users.noreply.github.com>
2024-08-23 00:24:58 +0800

    manually committed - certification not yet finished, need discussion for finalization

---
+++ src/pages/staff/Profile/StaffChooseImagebtn.jsx
@@ -0,0 +1,10 @@
+export default function ChooseImageButton() {
+    return (
+          <div className="flex items-center py-2 px-4 rounded-lg">
+            <button className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-green-300 transition">
+            Upload Image
+            </button>
+            </div>
+    );
+  }
+  
