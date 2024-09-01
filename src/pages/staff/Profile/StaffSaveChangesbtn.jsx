02706640639fa3894cc4a8823860f96efa8c1959
-- <kennbalino@gmail.com>
2024-09-01 20:07:21 +0800

    "current version for department head page"

--- src/pages/staff/Profile/StaffSaveChangesbtn.jsx
+++
@@ -1,8 +0,0 @@
-export default function StaffSaveChangesButton() {
-    return (
-      <button className="bg-blue-400 p-3 rounded-lg mr-5 text-white">
-        Save Changes
-      </button>
-    );
-  }
-  
