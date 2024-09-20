9b32c17a32b5d0ca32e43860618b1a96ae4d5dff
Raphael0603 <127576400+Raphael0603@users.noreply.github.com>
2024-09-21 00:18:29 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ContentDashStaff from "./pages/staff/Dashboard/StaffContentDash.jsx";
 import MainDashStaff from "./pages/staff/Dashboard/StaffMainDashboard.jsx";
 import OngoingStaff from "./pages/staff/Task/StaffOngoingTask.jsx";
-import CompletedStaff from "./pages/staff/History/CompletedTask.jsx";
 import StaffProfile from "./pages/staff/Profile/StaffPageProfile.jsx";
 import StaffUserInfo from "./pages/staff/Profile/StaffUserInfo.jsx";
 import StaffChangeAvatar from "./pages/staff/Profile/StaffAvatar.jsx";
 import StaffChangePassword from "./pages/staff/Profile/StaffChangePassword.jsx";
-
+import StaffPageCertificate from "./pages/staff/SendCert/StaffPageCert.jsx";
+import StaffSendCert from "./pages/staff/SendCert/StaffContentSendCert.jsx";
+import StaffImageContent from "./pages/staff/Task/StaffImageContent.jsx";
+import StaffImagePage from "./pages/staff/Task/StaffOngoingPage.jsx";
+import StaffHistoryPage from "./pages/staff/History/StaffHistoryPage.jsx";
 
 export default function App() {
   return (
@@ -25,18 +28,35 @@ export default function App() {
         >
           <Route path="home" element={<ContentDashStaff />} />
           {/* Routes for Ongoing */}
-            <Route path="ongoing" element={<OngoingStaff/>}>
+          <Route
+            path="StaffImagePage"
+            element={<StaffImagePage />}
+          >
+            <Route path="StaffImageContent" element={<StaffImageContent/>} />
+            
           </Route>
             {/* Completed Staff */}
-          <Route path="CompletedStaff" element={<CompletedStaff/>}>
-          </Route>
+             {/* Routes for History  */}
+             <Route
+              path="History"
+                element={<StaffHistoryPage />}
+              >
+                <Route path="StaffImageContent" element={<StaffImageContent/>} />
+                
+              </Route>
           {/* Routes for Profile */}
           <Route path="Staffprofile" element={<StaffProfile />}>
             <Route path="user_account" element={<StaffUserInfo />} /> 
             <Route path="Staffchange_avatar" element={<StaffChangeAvatar />} />
             <Route path="Staffchange_password" element={<StaffChangePassword />} />
           </Route>
-
+          {/* Routes for Sending Certificate  */}
+          <Route
+            path="StaffSendCert"
+            element={<StaffPageCertificate />}
+          >
+            <Route path="StaffCert" element={<StaffSendCert/>} />
+          </Route>
         </Route>
       </Routes>
     </Router>
