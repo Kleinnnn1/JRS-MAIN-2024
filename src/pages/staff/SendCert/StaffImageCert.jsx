4786a1116e854411932f1154061aa277dab608c2
Raphael0603 <127576400+Raphael0603@users.noreply.github.com>
2024-09-23 21:33:53 +0800

    Update StaffImageCert.jsx

--- src/pages/staff/SendCert/StaffImageCert.jsx
+++ src/pages/staff/SendCert/StaffImageCert.jsx
@@ -1,4 +1,4 @@
-import CertificateOfJobCompletion from "/src/assets/images/CertificateOfJobCompletion.png";
+import CertificateOfJobCompletion from "/src/assets/images/JRF.jpg";
 
 export default function ImageCertificate() {
   return (
