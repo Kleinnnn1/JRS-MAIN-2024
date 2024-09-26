ae0b83b65ce6b76ac3d8d80abe60767a649c2db4
Leokanette <ettenakoel@gmail.com>
2024-09-26 21:57:33 +0800

    update

--- src/service/apiReferral.js
+++ src/service/apiReferral.js
@@ -1,16 +1,8 @@
-import supabase from "./supabase";
+import { createClient } from "@supabase/supabase-js";
 
-export async function getReferral() {
-  const { data, error } = await supabase.from("referral").select("*");
+const supabaseUrl = "https://hnhwwcqwlcbcvbvryzxu.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhuaHd3Y3F3bGNiY3ZidnJ5enh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk2Njg1NzMsImV4cCI6MjAzNTI0NDU3M30.maYvTklRhmitbHy0lNvH5L0ePG0C1mknO2QOfYOIeCg";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-  if (error) {
-    console.error(error);
-    throw new Error("Referral could not be loaded");
-  }
-
-  return data;
-}
-
-// let { data: referral, error } = await supabase
-//   .from('referral')
-//   .select('*')
+export default supabase;
