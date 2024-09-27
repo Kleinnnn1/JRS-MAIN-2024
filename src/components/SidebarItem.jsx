dd7f69374095ba5c2b764889803d57230362d115
Leokanette <ettenakoel@gmail.com>
2024-09-27 14:09:33 +0800

    sidebar item fix

--- src/components/SidebarItem.jsx
+++ src/components/SidebarItem.jsx
@@ -1,15 +1,14 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-export default function SidebarItem({ name, onClick, image, isActive }) {
+
+export default function SidebarItem({ name, onClick, image, bold }) {
   return (
     <div
       onClick={onClick}
-      className={`flex items-center py-2 px-4 my-2 cursor-pointer rounded-md ${
-        isActive ? 'bg-custom-blue text-white font-bold' : 'text-gray-300 hover:bg-blue-980 hover:bg-yellow-600'
-      }`}
+      className={`group-[.active]:bg-custom-blue group-[.active]:text-white cursor-pointer flex items-center py-2 px-4 my-2 text-gray-300 hover:bg-yellow-500 hover:text-gray-100 rounded-md ${bold ? 'font-bold' : 'font-normal'}`}
     >
-      <img src={image} alt={`${name} Icon`} className=" " />
-      <span className="ml-2 hidden md:block">{name}</span>
+    <img src={image} alt={name} className="w-6 h-6"/>
+    <span className="ml-2 hidden md:block">{name}</span>
     </div>
   );
 }
@@ -17,7 +16,6 @@ export default function SidebarItem({ name, onClick, image, isActive }) {
 // Define PropTypes for the component
 SidebarItem.propTypes = {
   name: PropTypes.string.isRequired, // name is required and should be a string
-  onClick: PropTypes.func.isRequired, // onClick is required and should be a function
-  image: PropTypes.string.isRequired, // image is required and should be a string
-  isActive: PropTypes.bool // isActive is optional and should be a boolean
+  onClick: PropTypes.func, // onClick is optional and should be a function
+  image: PropTypes.string, // image is required and should be a string
 };
