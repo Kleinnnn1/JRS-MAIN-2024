e58ff73683d281f8eb9756406d3fb46142d5e917
-- <kennbalino@gmail.com>
2024-11-27 12:05:41 +0800

    "update button"

--- src/pages/system_admin/Users/addNewSpme.jsx
+++ src/pages/system_admin/Users/addNewSpme.jsx
@@ -188,7 +188,7 @@ function SysAdminAddNewSpme() {
               className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
               disabled={!isPasswordValid}
             >
-              Add Account
+              Submit
             </button>
           </div>
         </form>
