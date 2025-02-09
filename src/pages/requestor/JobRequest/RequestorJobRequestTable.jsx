5e612427d6bfe7460037b432cc926084586d63ea
-- <kennbalino@gmail.com>
2025-02-09 19:53:07 +0800

    added loading icon

--- src/pages/requestor/JobRequest/RequestorJobRequestTable.jsx
+++ src/pages/requestor/JobRequest/RequestorJobRequestTable.jsx
@@ -231,12 +231,38 @@ export default function RequestorJobRequestTable() {
 
       {error && <p className="text-red-600">{error}</p>}
 
-      <Table
-        columns={10}
-        rows={paginatedRequests.length}
-        content={tableContent}
-        headers={tableHeaders}
-      />
+      {loading ? (        // Kani ang loading ICON 
+        <div className="flex justify-center items-center py-10">
+          <svg
+            className="animate-spin h-10 w-10 text-blue-500"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8v8H4z"
+            ></path>
+          </svg>
+        </div>
+      ) : (
+        <Table
+          columns={10}
+          rows={paginatedRequests.length}
+          content={tableContent}
+          headers={tableHeaders}
+        />
+      )}
+
 
       <ReusablePagination
         rowsPerPage={rowsPerPage}
